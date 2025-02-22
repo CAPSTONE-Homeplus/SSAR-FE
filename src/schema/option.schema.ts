2a1da9fd8d9b38fcb39bc43c5f07a6796e573325
CrisBryann <nguyenphuocthienan2018cm@gmail.com>
2025-02-22 18:03:22 +0700

    update option and extra service

--- src/schema/option.schema.ts
+++ src/schema/option.schema.ts
@@ -1,30 +1,28 @@
 import { z } from "zod";
 import { BaseSchema } from "./base-schema";
 
-export const ExtraServiceSchema = BaseSchema.extend({
+export const OptionSchema = BaseSchema.extend({
   id: z.string().uuid(),
   name: z.string()
     .max(255, "Name must not exceed 255 characters")
     .min(3, "Name must be at least 3 characters long"),
   price: z.coerce.number()
     .positive("Price must be greater than 0")
-    .max(100000000, "Price must not exceed 100 million"), // Chuyển đổi từ string sang number
+    .max(100000000, "Price must not exceed 100 million"),
+  note: z.string().optional(),
   status: z.enum(["Active", "Inactive"], {
     errorMap: () => ({ message: "Status must be either 'Active' or 'Inactive'" }),
   }),
-  extraTime: z.coerce.number()
-    .int("Extra time must be an integer")
-    .positive("Extra time must be greater than 0")
-    .max(1440, "Extra time cannot exceed 24 hours (1440 minutes)"), // Chuyển đổi từ string sang number
+  isMandatory: z.boolean(),
+  maxQuantity: z.coerce.number().int().positive(),
+  discount: z.coerce.number().min(0, "Discount cannot be negative"),
   code: z.string()
     .max(50, "Code must not exceed 50 characters")
     .regex(/^[a-zA-Z0-9_-]+$/, "Code must only contain letters, numbers, underscores, or hyphens"),
   serviceId: z.string().uuid(),
 });
 
-
-
-export const ExtraServiceCreateSchema = BaseSchema.extend({
+export const OptionCreateSchema = BaseSchema.extend({
   name: z.string().max(255),
   description: z.string().optional(),
   price: z.number().positive(),
@@ -32,9 +30,9 @@ export const ExtraServiceCreateSchema = BaseSchema.extend({
   updatedBy: z.string().max(50),
 });
 
-export type TExtraServiceRequest = z.infer<typeof ExtraServiceSchema>;
-export type TExtraServiceResponse = z.infer<typeof ExtraServiceSchema>;
-export type TExtraServiceCreateRequest = z.infer<
-  typeof ExtraServiceCreateSchema
+export type TOptionRequest = z.infer<typeof OptionSchema>;
+export type TOptionResponse = z.infer<typeof OptionSchema>;
+export type TOptionCreateRequest = z.infer<
+  typeof OptionCreateSchema
 >;
-export type TExtraServiceUpdateRequest = z.infer<typeof ExtraServiceSchema>;
+export type TOptionUpdateRequest = z.infer<typeof OptionSchema>;
