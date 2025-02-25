1c8f77f53d5e4ac36618157f62a806651397e81f
CrisBryann <nguyenphuocthienan2018cm@gmail.com>
2025-02-25 22:24:32 +0700

    hi

--- src/apis/clients/auth.ts
+++ src/apis/clients/auth.ts
@@ -1,14 +1,15 @@
-import { httpLocal } from "@/lib/http";
-import { TAuthResponse } from "@/schema/auth.schema";
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { httpHomePlus } from "@/lib/http";
+import { TLoginRequest } from "@/schema/auth.schema";
 const authClient = {
-  auth: async (body: { user: TAuthResponse }) => {
-    return httpLocal.post("/api/auth", body);
+  auth: async (body: { user: TLoginRequest }) => {
+    return httpHomePlus.post("/auth/login", body);
   },
   logoutFromNextClientToNextServer: async (
     force?: boolean | undefined,
     signal?: AbortSignal | undefined
   ) => {
-    return httpLocal.post<any>(
+    return httpHomePlus.post<any>(
       "/api/auth/logout",
       {
         force,
