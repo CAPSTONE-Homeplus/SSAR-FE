6d81e7ebec12c58a3ecc4744f98e8b3ff9c3adbe
CrisBryann <nguyenphuocthienan2018cm@gmail.com>
2025-02-27 03:53:49 +0700

    test something will be convinent

--- src/app/(landing)/homeplus/layout.tsx
+++ src/app/(landing)/homeplus/layout.tsx
@@ -1,6 +1,8 @@
 import Header from "@/components/ui/header";
 import Footer from "@/components/ui/footer";
-import ChatIcons from "@/components/ui/chat";
+// import ChatIcons from "@/components/ui/chat";
+// import Chatbox from "@/components/ui/chatbox";
+import { ChatMessageListDemo } from "@/app/(landing)/homeplus/demo";
 
 export default function HomeLayout({
   children,
@@ -10,10 +12,15 @@ export default function HomeLayout({
   return (
     <>
       <Header />
-      <ChatIcons />
-      <main className="">
-        {children}
-      </main>
+      {/* <ChatIcons />
+      <div className="fixed bottom-50 right-5 flex flex-col items-end gap-2">
+        <Chatbox />
+      </div> */}
+      <div className="z-[10000]">
+        <ChatMessageListDemo />
+      </div>
+
+      <main className="">{children}</main>
       <Footer />
     </>
   );
